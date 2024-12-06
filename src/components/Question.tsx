2aa63bdb8c7a3bd6f1db29dcdc44df6c8f671007
weakbox <connormcleod2000@icloud.com>
2024-12-06 12:23:50 -0400

    Removed redundant state for tracking total points and animated counter using react-countup

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CountUp from "react-countup";
 import Answer from "./Answer";
 import styles from "./Question.module.css";
 
@@ -16,31 +17,34 @@ interface QuestionProps {
 
 function Question({question}: QuestionProps) {
   const [flipStates, setFlipStates] = useState(Array.from({length: question.answers.length}).fill(false));
-  const [totalPoints, setTotalPoints] = useState(0);
   
   // Runs when answer is clicked
-  function handleFlip(index: number, points: number) {
+  function handleFlip(index: number) {
     setFlipStates((prev) => (
       prev.map((flipped, i) => (i === index ? !flipped : flipped))  // State immutable so make a copy with map?
     ));
-
-    // TODO: make this happen on state change, not onclick
-    setTotalPoints((prev) => (
-      flipStates[index] ? prev - points : prev + points
-    ));
   }
+
+  // Not super sure why TypeScript needs a type annotation here
+  const totalPoints = flipStates.reduce((sum: number, flipState, index) => (
+    flipState ? sum + question.answers[index].points : sum
+  ), 0);
 
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.question}>{question.text}</h2>
-      <p className={styles.points}>{totalPoints}</p>
+      <CountUp
+        className={styles.points}
+        end={totalPoints}
+        preserveValue={true}
+      />
       {question.answers.map((answer, index) => (
         <Answer 
           key={index}
           answer={answer.text} 
           points={answer.points}
           flipped={flipStates[index] as boolean}
-          onFlip={() => handleFlip(index, answer.points)}
+          onFlip={() => handleFlip(index)}
         />
       ))}
     </div>
